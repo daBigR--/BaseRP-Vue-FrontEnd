ae6c3f2102967f3804c7304b83e21e7973216844
daBigR-- <rodrigo.pantoja@gmail.com>
2021-06-09 15:28:18 -0400

    login page using Prime Vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="login">
-    <h3>Login</h3>
-    <p><input
-      v-model="usr"
-      type="text"
-      name="usr"
-      placeholder="User name"
-    ></p>
-    <p><input
-      v-model="pwd"
-      type="password"
-      name="pwd"
-      placeholder="Password"
-    ></p>
-    <p><button @click="login">Login</button></p>
+<div class="container">
+  <div class="card p-fluid">
+    <h5>Login</h5>
+    <div class="p-field p-mt-5">
+      <span class="p-float-label">
+        <InputText id="usr" v-model="usr" type="text" class="p-d-box" />
+        <label for="usr">User name</label>
+      </span>
+    </div>
+    <div class="p-field p-mt-5">
+      <span class="p-float-label">
+      <InputText id="pwd" v-model="pwd" type="password" class="p-d-box" />
+      <label for="pwd">Password</label>
+      </span>
+    </div>
+    <div class="p-mt-5 login-button">
+      <Button label="Login" icon="pi pi-sign-in" @click="login" class="p-button-raised " />
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 export default {
   name: 'Login',
@@ -30,11 +36,34 @@ export default {
   },
   methods: {
     login() {
-      if (this.usr === this.pwd) {
+      if (this.usr && (this.usr === this.pwd)) {
         this.$store.commit('loggedIn');
-        this.$router.push({ name: 'Main' })
+        this.$router.push({ name: 'Main' });
       }
     }
+  },
+  components: {
+    Button,
+    InputText
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    margin: auto;
+    width: 300px;
+  }
+  .login-button {
+    width: 100px;
+    margin: auto;
+  }
+  @media (max-width: 600px) {
+  .container {
+    width: 80%;
+  }
+  .login-button {
+    width: 100%;
+  }
+  }
+</style>
